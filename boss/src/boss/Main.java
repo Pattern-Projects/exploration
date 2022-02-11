ea8059cb0d0be6bfd0f09513b506f0088b59ebed
Pattern-Projects <pattern.projects.hub@gmail.com>
2022-02-11 13:05:11 +0000

    Add uitls module Paths functionality

--- boss/src/boss/Main.java
+++ boss/src/boss/Main.java
@@ -1,10 +1,26 @@
 package boss;
 
+import java.io.IOException;
+
 import static utils.Paths.*;
 
 public class Main {
     public static void main(String... args) {
         System.out.println("boss main");
         test();
+
+        try {
+            connect();
+            System.out.println(pathIsSet());
+            System.out.println(pathExists());
+
+            setPath("C:\\Users\\publi\\Documents\\Java-Practice\\test_folder");
+            System.out.println(pathIsSet());
+            System.out.println(pathExists());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- utils/src/utils/Paths.java
+++ utils/src/utils/Paths.java
@@ -1,7 +1,45 @@
 package utils;
 
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Paths {
+    public static FileSystem fileSytem;
+    public static Path path;
     public static void test() {
         System.out.println("utils Paths.test()");
+    }
+
+    public static void connect() throws IOException {
+        System.out.println("Connecting to default file system:");
+        fileSytem = FileSystems.getDefault();
+    }
+
+    public static void setPath(String location) throws IOException{
+        try {
+            if (fileSytem == null) {
+                System.out.println("File System not initialised, calling connect()");
+                connect();
+            }
+
+            path = fileSytem.getPath(location);
+            System.out.println("Path Set.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean pathIsSet() {
+        if (path==null) return false;
+        else return true;
+    }
+
+    public static boolean pathExists() {
+        if (pathIsSet()) return Files.exists(path);
+        else return false;
     }
 }
