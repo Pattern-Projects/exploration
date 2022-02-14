c7382368585e10ff49288c3fd42a5b7846947efb
Pattern-Projects <pattern.projects.hub@gmail.com>
2022-02-14 10:58:57 +0000

    Create TextFile from BufferedWriter

--- boss/src/boss/Main.java
+++ boss/src/boss/Main.java
@@ -1,6 +1,11 @@
 package boss;
 
+import utils.TextFile;
+
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 import static utils.Paths.*;
 
@@ -8,6 +13,17 @@ public class Main {
     public static void main(String... args) {
         System.out.println("boss main");
         test();
+
+        var txt = new TextFile(Path.of("..\\test_folder\\abc"));
+        List<String> list = new ArrayList<>();
+        list.add("text file sup");
+        list.add("Well hello there");
+
+        try {
+            txt.append(list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         try {
             connect();
